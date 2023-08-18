39eba7f80e13ea95a4eef1e55bc940e94e40f030
Elias Kelta <eliaskelta1@gmail.com>
2023-08-18 18:29:14 +0200

    Reaktion auf Schimpfwörter hinzugefügt

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on('guildMemberAdd', member => {
+	console.log(member.tag);
+	console.log(member.guild.systemChannel);
+	member.guild.systemChannel.send('@' + member.tag + ' Hast du Problem?');
+});
+
+client.on('messageCreate', message => {
+	console.log(message.content);
+	schimpfwoerter = ['huan', 'hurensohn', 'nega', 'nigga', 'opfer', 'behinderter', 'behinderte'];
+	schimpfwoerter.forEach(wort => {
+		if (message.content.toLowerCase().includes(wort)) {
+			message.reply("Darf er so?");
+		}
+	});
 });
 
 // Log in to Discord with your client's token
